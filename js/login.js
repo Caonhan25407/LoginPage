deecd89bdc43d2123f3e910b1054989026092402
Nhan <caotranthiennhan250407@gmail.com>
2026-05-05 16:06:25 +0700

    upadte login.js

--- js/login.js
+++ js/login.js
@@ -33,22 +33,104 @@ $('#btnShowLogOut').click(function() {
 });
 
 //2. Đăng nhập 
-const loginEmailElement = document.getElementById('loginEmail');
-const loginPasswordElement = document.getElementById('loginPassword');
-const btnLogin = document.getElementById('btnLogin');
+// const loginEmailElement = document.getElementById('loginEmail');
+// const loginPasswordElement = document.getElementById('loginPassword');
+// const btnLogin = document.getElementById('btnLogin');
 
-btnLogin.addEventListener('click', (e) => {
+// btnLogin.addEventListener('click', (e) => {
+//     e.preventDefault();
+
+//     const loginEmail = loginEmailElement.value;
+//     const loginPassword = loginPasswordElement.value;
+
+//     if(loginEmail === "user@gmail.com" && loginPassword === "123456") {
+//         window.location.href = "./index.html";
+//     } else {
+//         loginEmailElement.style.borderColor = 'red';
+//         loginPasswordElement.style.borderColor = 'red';
+//     }
+//     console.log(loginEmailElement.value, loginPasswordElement.value);
+// });
+
+
+// REGISTER
+const registerForm = document.getElementById('registerForm');
+registerForm.addEventListener('submit', () => {
+    const name = document.getElementById('registerName').value;
+    const email = document.getElementById('registerMail').value.trim();
+    const password = document.getElementById('registerPassword').value;
+    const confirm = document.getElementById('registerConfirmPassword').value;
+
+    // validate
+    let isValid = true;
+    const errorName = document.getElementById('registerErrorNameForm');
+    const errorEmail = document.getElementById('registerErrorEmailForm');
+    const errorExists = document.getElementById('registerErrorEmail');
+
+    errorName.innerText = "";
+    errorEmail.innerText = "";
+    errorExists.innerText = "";
+
+    if (!name) {
+        errorName.innerText = "Please enter your name!";
+        isValid = false;
+    }
+
+    if (!email) {
+        errorEmail.innerText = "Please enter your email!";
+        isValid = false;
+    }
+
+    if (password !== confirm) {
+        isValid = false;
+    }
+
+    if (!isValid) return;
+
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+    // check trùng email
+    const exists = users.find(u => u.email === email);
+    if (exists) {
+        document.getElementById('registerErrorEmail').innerText = "Email exists";
+        return;
+    }
+
+    // thêm user
+    users.push({ name, email, password });
+    localStorage.setItem('users', JSON.stringify(users));
+    alert("Đăng ký thành công!");
+
+    // chuyển về login
+    registerBox.style.display = 'none';
+    loginBox.style.display = 'block';
+});
+
+// LOGIN
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const loginEmail = loginEmailElement.value;
-    const loginPassword = loginPasswordElement.value;
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
 
-    if(loginEmail === "user@gmail.com" && loginPassword === "123456") {
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+
+    const user = users.find(
+        u => u.email === email && u.password === password
+    );
+
+    if (user) {
+        // lưu trạng thái login
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        alert("Đăng nhập thành công!");
         window.location.href = "./index.html";
     } else {
-        loginEmailElement.style.borderColor = 'red';
-        loginPasswordElement.style.borderColor = 'red';
+        document.getElementById('loginEmail').style.borderColor = 'red';
+        document.getElementById('loginPassword').style.borderColor = 'red';
+        document.getElementById('errorPassword').innerText = "Incorrect email or password";
+
     }
-    console.log(loginEmailElement.value, loginPasswordElement.value);
 });
+
 
